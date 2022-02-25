42b673d2ba00b9cfd5a94b878519c3f5ac925108
Dan <d.lyu@mail.utoronto.ca>
2022-02-25 18:51:30 -0500

    Handle clickable header with styling

--- my-app/src/components/GridRow.js
+++ my-app/src/components/GridRow.js
@@ -1,14 +1,15 @@
 import * as React from 'react';
 
 export default function GridRow({rowId, values, headers, isHeader=false,
-                                    onClickHandler, clickableHeader}) {
+                                    onClickHandler, clickableHeader=[]}) {
 
     return (
         <tr key={rowId}>
             {
                 values.map((value, index) => {
                     const CTag = isHeader?`th`:`td`;
-                    return <CTag onClick={()=>onClickHandler(headers[index], value, rowId, index, isHeader)}
+                    const cellClass = clickableHeader.includes(headers[index])?'grid--clickable':''
+                    return <CTag className={cellClass} onClick={()=>onClickHandler(headers[index], value, rowId, index, isHeader)}
                                key={`${rowId}_${index}`}>{value}</CTag>
                 })
             }
