4742786fc1b495d3ba920e5da09db3b95311749c
Dan <d.lyu@mail.utoronto.ca>
2022-02-25 18:43:40 -0500

    Use full width for grid component

--- my-app/src/components/Grid.js
+++ my-app/src/components/Grid.js
@@ -2,38 +2,37 @@ import * as React from 'react';
 import '../styles/Grid.css';
 import GridRow from "./GridRow";
 
-export default function Grid({tableData, onClickHandler, excludeHeader=[]}) {
+export default function Grid({tableData, onClickHandler, excludeHeader = []}) {
     const headers = []
     tableData.forEach((item) => {
-        if(Object.keys(item).length > headers.length){
+        if (Object.keys(item).length > headers.length) {
             headers.length = 0
             for (let key in item) {
-                if(!excludeHeader.includes(key)){
+                if (!excludeHeader.includes(key)) {
                     headers.push(key)
                 }
             }
         }
     })
     return (
-        <div className="grid">
-            <table>
-                <tbody>
-                <GridRow key={-1} rowId={-1} headers={headers} values={headers} isHeader={true} onClickHandler={onClickHandler}/>
-                {tableData.map(value => {
-                    const rowValues = []
-                    headers.forEach((item) => {
-                        if(value[item]){
-                            rowValues.push(value[item])
-                        }else{
-                            rowValues.push("")
-                        }
-                    })
-                    return <GridRow key={value["rowId"]} rowId={value["rowId"]} values={rowValues}
-                                    headers={headers}
-                                    onClickHandler={onClickHandler}/>
-                })}
-                </tbody>
-            </table>
-        </div>
+        <table>
+            <tbody>
+            <GridRow key={-1} rowId={-1} headers={headers} values={headers} isHeader={true}
+                     onClickHandler={onClickHandler}/>
+            {tableData.map(value => {
+                const rowValues = []
+                headers.forEach((item) => {
+                    if (value[item]) {
+                        rowValues.push(value[item])
+                    } else {
+                        rowValues.push("")
+                    }
+                })
+                return <GridRow key={value["rowId"]} rowId={value["rowId"]} values={rowValues}
+                                headers={headers}
+                                onClickHandler={onClickHandler}/>
+            })}
+            </tbody>
+        </table>
     );
 }
