65e3ba251de0615469881badec1fa04a667b031b
Vicki Jin <jinsiqivicki@gmail.com>
2022-02-25 02:48:13 -0500

    create login, sign up file

---
+++ my-app/src/components/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import Sidebar from './Sidebar.js'
+
+function SignUp() {
+    return (
+        <React.Fragment>
+            <Sidebar />
+        </React.Fragment>
+    );
+}
+
+export default SignUp;
