bf5cbe348f1e263703c9e75ef9fda3b025aeadb8
Dan <d.lyu@mail.utoronto.ca>
2022-02-25 18:20:54 -0500

    Add Grid json handling

--- my-app/src/components/Grid.js
+++ my-app/src/components/Grid.js
@@ -2,14 +2,35 @@ import * as React from 'react';
 import '../styles/Grid.css';
 import GridRow from "./GridRow";
 
-export default function Grid({}) {
-
+export default function Grid({tableData}) {
+    const headers = []
+    tableData.forEach((item) => {
+        if(Object.keys(item).length > headers.length){
+            headers.length = 0
+            for (let key in item) {
+                headers.push(key)
+            }
+        }
+    })
+    console.log(headers)
     return (
         <div className="grid">
-                <table>
-                    <GridRow values={["test", "test"]} isHeader={true}/>
-                    <GridRow values={["test", "test"]}/>
-                </table>
+            <table>
+                <tbody>
+                <GridRow key={-1} uid={-1} values={headers} isHeader={true}/>
+                {tableData.map(value => {
+                    const rowValues = []
+                    headers.forEach((item) => {
+                        if(value[item]){
+                            rowValues.push(value[item])
+                        }else{
+                            rowValues.push("")
+                        }
+                    })
+                    return <GridRow key={value["rowId"]} uid={value["rowId"]} values={rowValues}/>
+                })}
+                </tbody>
+            </table>
         </div>
     );
 }
