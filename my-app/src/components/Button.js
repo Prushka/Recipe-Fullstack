ea13d3987e9950a350e7dd7dcdffda1d97af90e5
Dan <d.lyu@mail.utoronto.ca>
2022-02-25 21:40:57 -0500

    Add button background, border, text colors w/o mouse hover

--- my-app/src/components/Button.js
+++ my-app/src/components/Button.js
@@ -2,17 +2,49 @@ import * as React from 'react';
 import '../styles/Button.css';
 import {useState} from "react";
 
-export default function Button({
-                                   buttonBackgroundColor = '#8688BC',
-                                   buttonHoverBackgroundColor = '#5a5d95'
-                               }) {
+
+
+export function Button({
+                           textColor = 'white',
+                           buttonBackgroundColor = '',
+                           buttonHoverBackgroundColor = '',
+                           shadowOnHover = true,
+                           buttonBorderColor = '',
+                           buttonHoverBorderColor = '',
+                           onHover = () => {
+                           }, onClick = () => {
+    }
+                       }) {
+
 
     const [buttonBGColor, setButtonBGColor] = useState(buttonBackgroundColor)
+    const [buttonBColor, setButtonBColor] = useState(buttonBorderColor)
+    const [buttonShadow, setButtonShadow] = useState('')
     return (
-        <div className='button' style={{backgroundColor: buttonBGColor}}
-             onMouseOver={() => setButtonBGColor(buttonHoverBackgroundColor)}
-             onMouseLeave={() => setButtonBGColor(buttonBackgroundColor)}>
+        <div className={`button ${buttonShadow}`} style={{
+            color: textColor,
+            backgroundColor: buttonBGColor,
+            boxShadow: buttonShadow,
+            border: (buttonBorderColor || buttonHoverBorderColor) ? `2px solid ${buttonBColor}` : ''
+        }}
+             onMouseOver={() => {
+                 setButtonBGColor(buttonHoverBackgroundColor ? buttonHoverBackgroundColor : buttonBackgroundColor)
+                 setButtonBColor(buttonHoverBorderColor ? buttonHoverBorderColor : buttonBorderColor)
+                 if (shadowOnHover) {
+                     setButtonShadow('button--shadow')
+                 }
+                 onHover()
+             }}
+             onMouseLeave={() => {
+                 setButtonBGColor(buttonBackgroundColor)
+                 setButtonBColor(buttonBorderColor)
+                 if (shadowOnHover) {
+                     setButtonShadow('')
+                 }
+             }}
+             onClick={onClick}>
             sometext
         </div>
     );
 }
+
