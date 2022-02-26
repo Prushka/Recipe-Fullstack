d29f7f686ad725d6f6a69e5ac31a7792dfdddc94
Dan <d.lyu@mail.utoronto.ca>
2022-02-26 02:11:36 -0500

    Update TextField component className

--- my-app/src/components/TextField.js
+++ my-app/src/components/TextField.js
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import '../styles/Input.css';
-import {useState} from "react";
 
 export function TextField(props) {
     return (
-        <div style={{display: "flex", flexDirection: 'column'}}>
+        <div className={'input--text-group'}>
             {props.label && <label htmlFor={props.id}>{props.label}</label>}
             <input id={props.id} type="text" name="name"
                    placeholder={props.placeholder} style={{...props.style}}
