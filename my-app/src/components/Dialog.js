1b1e10399036677f60de2b3c6ab9b7d7fc4acfdc
Dan <d.lyu@mail.utoronto.ca>
2022-02-25 19:31:40 -0500

    Add closing functionality to dialog

--- my-app/src/components/Dialog.js
+++ my-app/src/components/Dialog.js
@@ -1,18 +1,22 @@
 import * as React from 'react';
 import '../styles/Dialog.css';
 
-export default function Dialog({size='m',
-                               minHeight='auto',
-                               minWidth='auto',
-                               maxHeight='auto',
-                               maxWidth='auto'}) {
+export default function Dialog({onClose, open, size = 'm',
+                                   minHeight = 'auto',
+                                   minWidth = 'auto',
+                                   maxHeight = 'auto',
+                                   maxWidth = 'auto'
+                               }) {
     return (
-        <div className='dialog'>
-            <div className={`modal modal--${size}`}
-            style={{minHeight:minHeight,
-            minWidth:minWidth,maxHeight:maxHeight,
-            maxWidth:maxWidth}}>
+        open ? <div className='dialog' onClick={onClose}>
+            <div onClick={(e) => {
+                e.stopPropagation();}} className={`modal modal--${size}`}
+                 style={{
+                     minHeight: minHeight,
+                     minWidth: minWidth, maxHeight: maxHeight,
+                     maxWidth: maxWidth
+                 }}>
             </div>
-        </div>
+        </div> : <></>
     );
 }
