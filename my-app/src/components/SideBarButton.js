72c4b8876e9fc656d0ad93d774d4a8bb8fe1c2b8
Dan <d.lyu@mail.utoronto.ca>
2022-02-26 05:13:55 -0500

    Allow passing styles to SideBarButton

--- my-app/src/components/SideBarButton.js
+++ my-app/src/components/SideBarButton.js
@@ -4,11 +4,11 @@ import React, {useState} from 'react';
 import '../styles/Sidebar.css';
 import {useNavigate} from 'react-router-dom';
 
-export default function SideBarButton({title, icon, path, isSelected}) {
+export default function SideBarButton({title, icon, path, isSelected, style}) {
     const navigate = useNavigate()
     return (
         <>
-            <button className={isSelected(path) && 'selected'}
+            <button style={{...style}} className={isSelected(path) && 'selected'}
                     onClick={() => navigate(path)}>{icon} {title}
             </button>
         </>
