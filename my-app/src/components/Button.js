64c1f1ae034fd78f16917314579b9e153987cc5b
Dan <d.lyu@mail.utoronto.ca>
2022-02-25 20:24:06 -0500

    Add button hover colors

--- my-app/src/components/Button.js
+++ my-app/src/components/Button.js
@@ -1,9 +1,17 @@
 import * as React from 'react';
 import '../styles/Button.css';
+import {useState} from "react";
 
-export default function Button({}) {
+export default function Button({
+                                   buttonBackgroundColor = '#8688BC',
+                                   buttonHoverBackgroundColor = '#5a5d95'
+                               }) {
+
+    const [buttonBGColor, setButtonBGColor] = useState(buttonBackgroundColor)
     return (
-        <div className='button'>
+        <div className='button' style={{backgroundColor: buttonBGColor}}
+             onMouseOver={() => setButtonBGColor(buttonHoverBackgroundColor)}
+             onMouseLeave={() => setButtonBGColor(buttonBackgroundColor)}>
             sometext
         </div>
     );
