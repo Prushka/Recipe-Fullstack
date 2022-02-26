495b2dc82965b591379070ffc33c2dc9b696b52e
Dan <d.lyu@mail.utoronto.ca>
2022-02-26 05:46:08 -0500

    Override image cell padding

--- my-app/src/components/GridRow.js
+++ my-app/src/components/GridRow.js
@@ -17,7 +17,7 @@ export default function GridRow({
                             child = <img className={'grid--avatar'} src={value} alt={value}/>
                         }
                     })
-                    return <CTag className={`cellClass`}
+                    return <CTag className={`cellClass ${child !== value && 'grid--avatar-container'}`}
                                  onClick={() => onClickHandler(headers[index], value, id, index, isHeader)}
                                  key={`${id}_${index}`}>{child}</CTag>
                 })
