108ab0d500cbe34b146c01e58b38c14d365a4e2e
Dan <d.lyu@mail.utoronto.ca>
2022-02-25 20:20:05 -0500

    Add button component and style

---
+++ my-app/src/components/Button.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import '../styles/Button.css';
+
+export default function Button({}) {
+    return (
+        <div className='button'>
+            sometext
+        </div>
+    );
+}
