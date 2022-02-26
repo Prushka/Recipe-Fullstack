798327ac6b283d26318dbc08e6860551a762508a
Dan <d.lyu@mail.utoronto.ca>
2022-02-25 21:41:12 -0500

    Add 3 styled buttons

--- my-app/src/components/Button.js
+++ my-app/src/components/Button.js
@@ -2,6 +2,34 @@ import * as React from 'react';
 import '../styles/Button.css';
 import {useState} from "react";
 
+export function RedBGButton({children, onClick}) {
+    return (<Button
+        buttonBackgroundColor={'#FC5185'}
+        buttonHoverBackgroundColor={'#dc4674'}
+        onClick={onClick}
+    > {children} </Button>);
+}
+
+
+export function GreyBorderRedButton({children, onClick}) {
+    return (<Button
+        buttonBorderColor={'#979797'}
+        buttonHoverBorderColor={'#777777'}
+        onClick={onClick}
+        shadowOnHover={false}
+        textColor={'var(--theme-1)'}
+    > {children} </Button>);
+}
+
+export function BlueBGButton({children, onClick}) {
+    return (
+        <Button
+            buttonBackgroundColor={'#8688BC'}
+            buttonHoverBackgroundColor={'#7677ad'}
+            onClick={onClick}
+        > {children} </Button>
+    );
+}
 
 
 export function Button({
