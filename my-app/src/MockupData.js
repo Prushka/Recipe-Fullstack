8c0e84c8dad0aade27cd53f5c91273e9af9f077d
Dan <d.lyu@mail.utoronto.ca>
2022-02-26 06:29:51 -0500

    Add reviews mockup data

--- my-app/src/MockupData.js
+++ my-app/src/MockupData.js
@@ -2,20 +2,25 @@ const users = {
     "TestUser1": {
         "Username": "TestUser1",
         "Permission": "Guest",
-        "id": 1
+        "id": 1,
+        "Email": "testuser1@example.com",
+        "Uploaded Recipes": null
     },
     "TestUser2": {
         "Username": "TestUser2",
         "Permission": "User",
         "id": 2,
-        "avatar": "https://s2.loli.net/2022/02/11/1IV4f92WzuUYKcm.jpg"
+        "avatar": "https://s2.loli.net/2022/02/11/1IV4f92WzuUYKcm.jpg",
+        "Uploaded Recipes": 6
     }
     ,
     "TestUser3": {
         "Username": "TestUser3",
         "Permission": "Admin",
         "id": 3,
-        "avatar": "https://s2.loli.net/2022/02/10/grldkO4LeDjxmY8.png"
+        "avatar": "https://s2.loli.net/2022/02/10/grldkO4LeDjxmY8.png",
+        "Email": "testuser3@example.com",
+        "Uploaded Recipes": 3
     }
 }
 
@@ -47,10 +52,31 @@ const recipes = [
     }
 ]
 
+const reviews = [
+    {
+        "Recipe": "Water",
+        "Recipe Author": "TestUser2",
+        "Rating": 2,
+        "Rating Author": "TestUser3",
+        "Report Count": 20,
+        "Posted At": "2022-01-20",
+        "id": 0
+    },
+    {
+        "Recipe": "Sushi",
+        "Recipe Author": "TestUser1",
+        "Rating": 5,
+        "Rating Author": "TestUser3",
+        "Report Count": 0,
+        "Posted At": "2022-01-21",
+        "id": 1
+    }
+]
+
 const defaultUser = {
     "Username": "None",
     "Permission": "None",
     "id": -1
 }
 
-export {recipes, users, defaultUser}
+export {recipes, users, defaultUser, reviews}
