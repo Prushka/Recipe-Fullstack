ce990457cda72d01a5347133e1e4f5d47b73289d
Dan <d.lyu@mail.utoronto.ca>
2022-02-25 19:25:28 -0500

    Add min/max width/height props to modal

--- my-app/src/components/Dialog.js
+++ my-app/src/components/Dialog.js
@@ -1,10 +1,18 @@
 import * as React from 'react';
 import '../styles/Dialog.css';
 
-export default function Dialog({}) {
+export default function Dialog({size='m',
+                               minHeight='auto',
+                               minWidth='auto',
+                               maxHeight='auto',
+                               maxWidth='auto'}) {
     return (
         <div className='dialog'>
-            <div className='modal modal--l'></div>
+            <div className={`modal modal--${size}`}
+            style={{minHeight:minHeight,
+            minWidth:minWidth,maxHeight:maxHeight,
+            maxWidth:maxWidth}}>
+            </div>
         </div>
     );
 }
