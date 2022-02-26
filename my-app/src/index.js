a0b5709a9233a5d0ed8e6a746ea5cabec419d1bc
Dan <d.lyu@mail.utoronto.ca>
2022-02-26 04:44:49 -0500

    Update sidebar to use navigate routing

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,18 +7,24 @@ import Profile from "./components/Profile";
 import BrowseRecipe from "./components/BrowseRecipe";
 import SavedRecipe from "./components/SavedRecipe";
 import UploadRecipe from "./components/UploadRecipe";
+import Sidebar from "./components/Sidebar";
+import Admin from "./pages/Admin";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />}>
-        <Route path="profile" element={<Profile />} />
-        <Route path="browse" element={<BrowseRecipe />} />
-        <Route path="saved" element={<SavedRecipe />} />
-        <Route path="upload" element={<UploadRecipe />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+    <>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path="profile" element={<Profile/>}/>
+                <Route path="browse" element={<BrowseRecipe/>}/>
+                <Route path="saved" element={<SavedRecipe/>}/>
+                <Route path="upload" element={<UploadRecipe/>}/>
+                <Route path="/manage/users" element={<><Sidebar userIsAdmin={true}/><Admin/></>}/>
+                <Route path="/manage/recipes" element={<><Sidebar userIsAdmin={true}/><Admin/></>}/>
+                <Route path="/manage/reviews" element={<><Sidebar userIsAdmin={true}/><Admin/></>}/>
+            </Routes>
+        </BrowserRouter>
+    </>,
   document.getElementById('root')
 );
 
