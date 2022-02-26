18e8b5c10c98f36738ac7cde69b1e5e603e1d138
Dan <d.lyu@mail.utoronto.ca>
2022-02-26 00:04:39 -0500

    Add TextField component with styling

---
+++ my-app/src/components/TextField.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import '../styles/Input.css';
+import {useState} from "react";
+
+export function TextField(props) {
+
+return (
+    <div style={{display: "flex", flexDirection: 'column'}}>
+        {props.label && <label htmlFor={props.id}>{props.label}</label>}
+        <input id={props.id} type="text" name="name"
+               placeholder={props.placeholder}/>
+    </div>
+    );
+}
+
