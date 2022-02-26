ea4af77011d27e97e833e2efc0604700896ab190
Dan <d.lyu@mail.utoronto.ca>
2022-02-26 00:18:47 -0500

    Update TextField focus and styling

--- my-app/src/components/TextField.js
+++ my-app/src/components/TextField.js
@@ -3,13 +3,13 @@ import '../styles/Input.css';
 import {useState} from "react";
 
 export function TextField(props) {
-
-return (
-    <div style={{display: "flex", flexDirection: 'column'}}>
-        {props.label && <label htmlFor={props.id}>{props.label}</label>}
-        <input id={props.id} type="text" name="name"
-               placeholder={props.placeholder}/>
-    </div>
+    return (
+        <div style={{display: "flex", flexDirection: 'column'}}>
+            {props.label && <label htmlFor={props.id}>{props.label}</label>}
+            <input id={props.id} type="text" name="name"
+                   placeholder={props.placeholder} style={{...props.style}}
+                   />
+        </div>
     );
 }
 
