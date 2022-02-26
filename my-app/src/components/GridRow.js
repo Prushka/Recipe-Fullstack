754dbc10747533107774ffd5f223db394163d09d
Dan <d.lyu@mail.utoronto.ca>
2022-02-26 05:45:00 -0500

    Allow passing images urls to grid cells

--- my-app/src/components/GridRow.js
+++ my-app/src/components/GridRow.js
@@ -1,16 +1,25 @@
 import * as React from 'react';
 
-export default function GridRow({id, values, headers, isHeader=false,
-                                    onClickHandler, clickableHeader=[]}) {
-
+export default function GridRow({
+                                    id, values, headers, isHeader = false,
+                                    onClickHandler, clickableHeader = []
+                                }) {
+    const images = ['.jpg', '.png']
     return (
         <tr key={id}>
             {
                 values.map((value, index) => {
-                    const CTag = isHeader?`th`:`td`;
-                    const cellClass = clickableHeader.includes(headers[index])?'grid--clickable':''
-                    return <CTag className={cellClass} onClick={()=>onClickHandler(headers[index], value, id, index, isHeader)}
-                               key={`${id}_${index}`}>{value}</CTag>
+                    const CTag = isHeader ? `th` : `td`;
+                    const cellClass = clickableHeader.includes(headers[index]) ? 'grid--clickable' : ''
+                    let child = value
+                    images.forEach((image) => {
+                        if (value.endsWith(image)) {
+                            child = <img className={'grid--avatar'} src={value} alt={value}/>
+                        }
+                    })
+                    return <CTag className={`cellClass`}
+                                 onClick={() => onClickHandler(headers[index], value, id, index, isHeader)}
+                                 key={`${id}_${index}`}>{child}</CTag>
                 })
             }
         </tr>
