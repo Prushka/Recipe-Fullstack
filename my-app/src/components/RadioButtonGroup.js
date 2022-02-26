3250fff89f9fcd4569a7e0adaca27eab45159702
Dan <d.lyu@mail.utoronto.ca>
2022-02-26 02:10:39 -0500

    Add radio button group component and styling

---
+++ my-app/src/components/RadioButtonGroup.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import '../styles/Input.css';
+import GridRow from "./GridRow";
+
+export function RadioButtonGroup({
+                                     title,
+                                     options,
+     style
+                                 }) {
+    return (
+        <>
+            <radio-section style={{...style}}>
+                <radio-section-title>{title}</radio-section-title>
+                <radio-group>
+                    {options.map(option => {
+                        return (
+                            <radio-option>
+                                <span><input type={"radio"} id={option} name={title}/></span>
+                                <radio-label>{option}</radio-label>
+                            </radio-option>
+                        )
+
+                    })}
+                </radio-group>
+            </radio-section>
+
+        </>
+
+    );
+}
+
