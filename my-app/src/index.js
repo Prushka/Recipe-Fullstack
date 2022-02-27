2399fb65e44f983003c2bbaf2d0811139bc3eb32
Vicki Jin <jinsiqivicki@gmail.com>
2022-02-27 09:57:43 -0500

    change format

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import Home from './components/Home';
+import Home from './components/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Profile from "./components/Profile";
 import BrowseRecipe from "./components/BrowseRecipe";
@@ -9,11 +9,14 @@ import SavedRecipe from "./components/SavedRecipe";
 import UploadRecipe from "./components/UploadRecipe";
 import Login from "./components/LogIn_SignUp/Login";
 import SignUp from "./components/LogIn_SignUp/SignUp";
+import Sidebar from "../src/components/Sidebar";
 
 ReactDOM.render(
   <>
     <BrowserRouter>
             <Routes>
+                <Route path="home" element={<Home/>}/>
+                <Route path="sidebar" element={<Sidebar/>}/>
                 <Route path="profile" element={<Profile/>}/>
                 <Route path="browse" element={<BrowseRecipe/>}/>
                 <Route path="saved" element={<SavedRecipe/>}/>
