12e06ae94b5e58ffd04925adcb708d887a8c186b
Vicki Jin <jinsiqivicki@gmail.com>
2022-02-27 16:22:31 -0500

    saved recipie basic feature

--- my-app/src/components/SavedRecipe/SavedRecipe.js
+++ my-app/src/components/SavedRecipe/SavedRecipe.js
@@ -1,13 +1,34 @@
-import React from 'react'; 
+import React from 'react';
 import '../SavedRecipe/SavedRecipe.css';
 import Sidebar from '../Sidebar.js';
 import '../../styles/Sidebar.css';
 
 class SavedRecipe extends React.Component {
-    render(){
-        return(
+    render() {
+        return (
             <React.Fragment>
                 <Sidebar />
+
+                <div id='saved'>
+                    <div class='header'>
+                        <p>Saved Recipes</p>
+                    </div>
+
+                    <div class='savedRecipeList'>
+
+                        <div class="savedContent">
+                            <h5>Recipe2</h5>
+                            <img class="recipePicture" src="food.jpg" alt="" />
+                        </div>
+                    </div>
+
+                    <div class='savedRecipeList'>
+                        <div class="savedContent">
+                            <h5>Recipe2</h5>
+                            <img class="recipePicture" src="food.jpg" alt="" />
+                        </div>
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
