5ae4a717917d40b482d3770cfea5754090a3eb1b
Dan <d.lyu@mail.utoronto.ca>
2022-02-27 15:47:59 -0500

    Update TextField mobile style

--- my-app/src/components/input/TextField.js
+++ my-app/src/components/input/TextField.js
@@ -7,12 +7,12 @@ import './Input.css';
 
 export function TextField(props) {
     return (
-        <textfield-section style={{...props.style}}>
+        <div className={'textfield-section'} style={{...props.style}}>
             {props.label && <label htmlFor={props.id}>{props.label}</label>}
             <input id={props.id} type="text" name={props.name}
                    placeholder={props.placeholder} {...props}
                    />
-        </textfield-section>
+        </div>
     );
 }
 
