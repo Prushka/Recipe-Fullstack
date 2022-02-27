dec8739ccb25b42c12dcd3d15d71c329cc5bf4c0
Vicki Jin <jinsiqivicki@gmail.com>
2022-02-27 04:41:34 -0500

    login finish

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,32 +1,42 @@
-import React from 'react';
-
+import React from "react";
+import "./login.css";
 class Login extends React.Component {
-    state = {
-        username: '',
-        pwd: ''
-
-    }
-    render() {
-        return (
-            <React.Fragment>
-                <div>
-                    <h1>Log-in</h1>
-                </div>
-                <div>
-                    <form>
-                        <input type='username' name='username' placeholder='username...' required />
-
-                    </form>
-                    <form>
-                        <input type='password' name='pwd' placeholder='password...' required />
-                    </form>
-                    <form>
-                        <button>Log-in</button>
-                    </form>
-                </div>
-            </React.Fragment>
-        )
-    }
+  state = {
+    username: "",
+    pwd: ""
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <div className="login">
+          <div className="login_banner">
+            <p>Recipe...</p>
+          </div>
+          <div className="login_box">
+            <form>
+              <input
+                type="username"
+                name="username"
+                placeholder="username..."
+                required
+              />
+            </form>
+            <form>
+              <input
+                type="password"
+                name="pwd"
+                placeholder="password..."
+                required
+              />
+            </form>
+          </div>
+          <div>
+            <button>LOGIN</button>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Login;
