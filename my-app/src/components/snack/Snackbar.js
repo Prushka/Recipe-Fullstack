f4e67dff5ec60807ad26252f8a34b00ab7f58460
Dan <d.lyu@mail.utoronto.ca>
2022-02-28 11:50:19 -0500

    Add Snackbar timeout and its prop

--- my-app/src/components/snack/Snackbar.js
+++ my-app/src/components/snack/Snackbar.js
@@ -4,16 +4,24 @@ import {IoClose} from "react-icons/io5";
 import {useState} from "react";
 
 
-export function Snackbar({open = true,
-                         position="bottom-middle",
-                         type="error"}) {
+export function Snackbar({
+                             open = true,
+                             position = "bottom-middle",
+                             type = "error",
+                             timeout = 5000
+                         }) {
     const [snackBarOpen, setSnackbarOpen] = useState(open);
+    if (timeout > 0) {
+        setTimeout(() => {
+            setSnackbarOpen(false);
+        }, timeout);
+    }
     return (
         <div>
             {snackBarOpen ? <div className={`snackbar snackbar--${position} snackbar--${type}`}>
                 <span>test</span>
                 <IoClose
-                    onClick={()=>setSnackbarOpen(false)} size={25} className={'button-icon snackbar--close'}/>
+                    onClick={() => setSnackbarOpen(false)} size={25} className={'button-icon snackbar--close'}/>
             </div> : <></>}
 
         </div>
