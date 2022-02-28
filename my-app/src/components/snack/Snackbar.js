7c86351d9fc2bff0b22f08d939d751e903d96941
Dan <d.lyu@mail.utoronto.ca>
2022-02-28 11:48:47 -0500

    Add success, default and error Snackbar styles

--- my-app/src/components/snack/Snackbar.js
+++ my-app/src/components/snack/Snackbar.js
@@ -5,11 +5,12 @@ import {useState} from "react";
 
 
 export function Snackbar({open = true,
-                         position="bottom-middle"}) {
+                         position="bottom-middle",
+                         type="error"}) {
     const [snackBarOpen, setSnackbarOpen] = useState(open);
     return (
         <div>
-            {snackBarOpen ? <div className={`snackbar snackbar--${position}`}>
+            {snackBarOpen ? <div className={`snackbar snackbar--${position} snackbar--${type}`}>
                 <span>test</span>
                 <IoClose
                     onClick={()=>setSnackbarOpen(false)} size={25} className={'button-icon snackbar--close'}/>
