14c80950684d32e46a536324dd6873296021a3ae
Dan <d.lyu@mail.utoronto.ca>
2022-02-28 12:19:09 -0500

    Add multiple Snackbars stacking functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,14 +9,16 @@ import UploadRecipe from "./components/UploadRecipe";
 import SideBar from "./pages/SideBar";
 import {AdminManageRecipes, AdminManageUsers, AdminManageReviews} from "./pages/Admin";
 import TopBar from "./pages/TopBar";
-import {Snackbar} from "./components/snack/Snackbar";
+import {getSnackBarNewId, Snackbar} from "./components/snack/Snackbar";
 
 
 export default function App() {
     const [sideBarOpen, setSideBarOpen] = useState(false);
     const PageComponent = ({path, children}) => {
         return (<>
-            <Snackbar />
+            <Snackbar snackbarId={getSnackBarNewId()}/>
+            <Snackbar snackbarId={getSnackBarNewId()}/>
+            <Snackbar snackbarId={getSnackBarNewId()}/>
             <div className={`${sideBarOpen ? 'side-bar-overlay' : ''}`} onClick={() => setSideBarOpen(false)}/>
             <TopBar sideBarOpen={sideBarOpen} setSideBarOpen={setSideBarOpen}/>
             <div className={'page-body'}>
--- my-app/src/components/snack/Snackbar.js
+++ my-app/src/components/snack/Snackbar.js
@@ -1,29 +1,54 @@
 import * as React from 'react';
 import './Snackbar.css';
 import {IoClose} from "react-icons/io5";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
+let snackbarIds = 0
+const snackbars = new Set()
+const getSnackBarNewId = () => {
+    snackbarIds = snackbarIds + 1
+    return snackbarIds
+}
+
+export {getSnackBarNewId}
 
 export function Snackbar({
                              open = true,
                              position = "bottom-middle",
                              type = "error",
-                             timeout = 5000
+                             timeout = -1,
+                             snackbarId
                          }) {
-    const [snackBarOpen, setSnackbarOpen] = useState(open);
+    const [snackBarOpen, setSnackbarOpen] = useState(open)
+    snackbars.add(snackbarId)
+    const [append] = useState([...snackbars].indexOf(snackbarId) * 65)
+    const checkSet = (newState) => {
+        // I'm not sure if setState will update if the value's the same
+        // This is just to make sure we don't trigger useEffect multiple times
+        if (snackBarOpen === newState) {
+            return
+        }
+        setSnackbarOpen(newState)
+    }
+    useEffect(() => {
+        if (!snackBarOpen) {
+            snackbars.delete(snackbarId)
+        }
+    }, [snackbarId, snackBarOpen])
+    console.log(snackbars)
     if (timeout > 0) {
         setTimeout(() => {
-            setSnackbarOpen(false);
+            checkSet(false);
         }, timeout);
     }
     return (
         <div>
-            {snackBarOpen ? <div className={`snackbar snackbar--${position} snackbar--${type}`}>
-                <span>test</span>
-                <IoClose
-                    onClick={() => setSnackbarOpen(false)} size={25} className={'button-icon snackbar--close'}/>
-            </div> : <></>}
-
+            {snackBarOpen ?
+                <div style={{bottom: `${append}px`}} className={`snackbar snackbar--${position} snackbar--${type}`}>
+                    <span>test</span>
+                    <IoClose
+                        onClick={() => checkSet(false)} size={25} className={'button-icon snackbar--close'}/>
+                </div> : <></>}
         </div>
     )
 }
