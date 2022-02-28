9c79d9698ebcd0e5508efd485ca995551bc5bf17
Dan <d.lyu@mail.utoronto.ca>
2022-02-28 11:46:11 -0500

    Add top/bottom-left/right/middle 6 position snackbar styles

--- my-app/src/components/snack/Snackbar.js
+++ my-app/src/components/snack/Snackbar.js
@@ -1,15 +1,20 @@
 import * as React from 'react';
 import './Snackbar.css';
 import {IoClose} from "react-icons/io5";
+import {useState} from "react";
 
 
-export function Snackbar({}) {
+export function Snackbar({open = true,
+                         position="bottom-middle"}) {
+    const [snackBarOpen, setSnackbarOpen] = useState(open);
     return (
-        <>
-            <div className={'snackbar'}>
+        <div>
+            {snackBarOpen ? <div className={`snackbar snackbar--${position}`}>
                 <span>test</span>
-                <IoClose size={25} className={'button-icon snackbar--close'}/>
-            </div>
-        </>
+                <IoClose
+                    onClick={()=>setSnackbarOpen(false)} size={25} className={'button-icon snackbar--close'}/>
+            </div> : <></>}
+
+        </div>
     )
 }
