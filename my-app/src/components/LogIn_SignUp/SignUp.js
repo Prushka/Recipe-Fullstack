f534a2294faafa77f69fb9bf1a3c7da21ebf6fcf
Vicki Jin <jinsiqivicki@gmail.com>
2022-03-04 15:41:00 -0500

    fixed warining

--- my-app/src/components/LogIn_SignUp/SignUp.js
+++ my-app/src/components/LogIn_SignUp/SignUp.js
@@ -6,8 +6,8 @@ import useForm from './useForm';
 import validate from './ValidateInfoCheck';
 
 
-const SignUp =()=> {
-    const { handleChange,handelSubmit,values,errors}=useForm(validate);
+const SignUp = () => {
+    const { handleChange, handelSubmit, values, errors } = useForm(validate);
     /*state = {
         username: "",
         email: "",
@@ -15,75 +15,76 @@ const SignUp =()=> {
         pwdConfirmation: ""
     };*/
     //render() {
-        return (
-            <React.Fragment>
-                <div className="login">
-                    <div className="login_banner">
-                        <p>Recipe...</p>
-                    </div>
-                    <div>
-                        <h1>Sign-Up</h1>
-                    </div>
-                    <div className="form-inputs">
-                        <form className='form' onSubmit={handelSubmit}>
-                            <input
-                                type="username"
-                                name="username"
-                                placeholder="username..."
-                                required
-                                value={values.username}
-                                onChange={handleChange}
-                            />
-                        </form>
-                        <form>
-                            <input
-                                type="email"
-                                name="email"
-                                placeholder="email..."
-                                required
-                                value={values.email}
-                                onChange={handleChange}
-                            />
-                            {errors.username &&<p>{errors.username}</p>}
-                        </form>
-                        <form>
-                            <input
-                                type="password"
-                                name="pwd"
-                                placeholder="password..."
-                                required
-                                value={values.pwd}
-                                onChange={handleChange}
-                            />
-                        </form>
-                        <form>
-                            <input
-                                type="password"
-                                name="pwd2"
-                                placeholder="comfirm password..."
-                                required
-                                value={values.pwd2}
-                                onChange={handleChange}
-                            />
-                        {errors.pwd2 && <p>{errors.pwd2}</p>}
-                        </form>
-                    </div>
-                    <div>
-                        <Link to={"/home"}>
-                            <button >SUBMIT</button>
-                        </Link>
-
-                    </div>
-                    <div className='back'>
-                        <Link to={"/login"} style={{ textDecoration: 'none' }}>
-                            <button>Back to login</button>
-                        </Link>
-                        
-                    </div>
+    return (
+        <React.Fragment>
+            <div className="login">
+                <div className="login_banner">
+                    <p>Recipe...</p>
                 </div>
-            </React.Fragment>
-        );
-    }
+                <div>
+                    <h1>Sign-Up</h1>
+                </div>
+                <div className="form-inputs">
+                    <form className='form' onSubmit={handelSubmit}>
+                        <input
+                            type="username"
+                            name="username"
+                            placeholder="username..."
+                            required
+                            value={values.username}
+                            onChange={handleChange}
+                        />
+                    </form>
+                    <form>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="email..."
+                            required
+                            value={values.email}
+                            onChange={handleChange}
+                        />
+                        {errors.username && <p>{errors.username}</p>}
+                    </form>
+                    <form>
+                        <input
+                            type="password"
+                            name="pwd"
+                            placeholder="password..."
+                            required
+                            value={values.pwd}
+                            onChange={handleChange}
+                        />
+                    </form>
+                    <form>
+                        <input
+                            type="password"
+                            name="pwd2"
+                            placeholder="comfirm password..."
+                            required
+                            value={values.pwd2}
+                            onChange={handleChange}
+                        />
+                        {errors.pwd2 && <p>{errors.pwd2}</p>}
+                    </form>
+                </div>
+                <div>
+                    {/* <Link to={"/home"}>
+                            <button >SUBMIT</button>
+        </Link>*/}
+
+                    <button >SUBMIT</button>
+                </div>
+                <div className='back'>
+                    <Link to={"/login"} style={{ textDecoration: 'none' }}>
+                        <button>Back to login</button>
+                    </Link>
+
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
 //}
 
 export default SignUp;
