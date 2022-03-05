2f62bc0c2f87cee25300539250abdd5e0cc5057f
Vicki Jin <jinsiqivicki@gmail.com>
2022-03-05 17:41:45 -0500

    hardcode userInfo

---
+++ my-app/src/components/LogIn_SignUp/userInfo.js
@@ -0,0 +1,18 @@
+const userInfo = [
+    {
+        "username": "user1",
+        "email": "user1@user.com",
+        "pwd": "pwd1"
+
+    },
+    {
+        "username": "user2",
+        "email": "user2@user.com",
+        "pwd": "pwd2"
+    },
+    {
+        "username": "user3",
+        "email": "user3@user.com",
+        "pwd": "pwd3"
+    }
+]
