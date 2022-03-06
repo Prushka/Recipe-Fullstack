53f691a75cb39eff4288ae30dc49bcd09f74f0c1
Vicki Jin <jinsiqivicki@gmail.com>
2022-03-06 11:54:06 -0500

    show error msg for unmatch pwd & name

--- my-app/src/components/LogIn_SignUp/Login.js
+++ my-app/src/components/LogIn_SignUp/Login.js
@@ -5,49 +5,51 @@ import { Link } from 'react-router-dom';
 class Login extends React.Component {
     state = {
         username: "",
-        pwd: ""
+        password: "",
+        error: false,
     };
 
-    handleChange = e =>{
-        const target = e.target
+    handleChange(event) {
+        const target = event.target
         const value = target.value
         const name = target.name
-        this.setState({[name]:value})
+        this.setState({ [name]: value })
     };
 
-    handelClick = e =>{
+    handelClick(event) {
         //hard code database
         const user_date = [
             {
-                username:"user1",
-                password:"user1"
+                username: "user1",
+                password: "user1"
             },
             {
-                username:"user2",
-                password:"user2"
+                username: "user2",
+                password: "user2"
             }
         ];
 
         //check existence of username
-        const checkExist = user_date.find((user)=>user.username===this.state.username);
+        const checkExist = user_date.find((user) => user.username === this.state.username);
 
-        if(!checkExist){
+        if (!checkExist) {
             console.log("user does not exist")
             this.setState({
-                error:true
+                error: true
             })
-        }else{
-            if(checkExist.password !== this.state.password){
+        } else {
+            if (checkExist.password !== this.state.password) {
                 console.log("wrong password")
                 this.setState({
-                    error:true
+                    error: true
                 })
             }
         }
     }
-    
+
 
     render() {
+
         return (
             <React.Fragment>
                 <div className="login">
@@ -61,22 +63,32 @@ class Login extends React.Component {
                                 type="username"
                                 name="username"
                                 placeholder="username..."
+                                className='input'
+                                value={this.state.username}
+                                onChange={event => this.handleChange(event)}
                                 required
                             />
                         </form>
                         <form>
                             <input
                                 type="password"
-                                name="pwd"
+                                name="password"
                                 placeholder="password..."
+                                className='input'
+                                value={this.state.password}
+                                onChange={event => this.handleChange(event)}
                                 required
                             />
                         </form>
                     </div>
                     <div>
-                        <Link to={"/home"}>
-                            <button>LOGIN</button>
-                        </Link>
+                        <button type='submit' name='submit' className='form_login'
+                            onClick={event => this.handelClick(event)}>
+                            LOG-IN
+                        </button>
+
+                        {this.state.error ? <div className='error_msg'>username and password unmatch</div> : <Link to={'/home'} />}
+
 
                     </div>
                 </div>
