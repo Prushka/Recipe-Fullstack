c50b31e0d0e1d61d5793e187333010a4212a8ad7
Dan <d.lyu@mail.utoronto.ca>
2022-03-30 12:33:06 -0400

    Implement sign up route

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response} from 'express';
 import * as bodyParser from "body-parser";
-import {UserModel} from "./models/recipe";
+import {User} from "./models/recipe";
 import connectMongo from "./db/mongoose";
 
 connectMongo().catch(err => console.log(err))
@@ -13,22 +13,36 @@ function isValidationError(error: Error) {
     return error.name === "ValidationError"
 }
 
+function serverError(res: Response) {
+    res.status(500).send("Internal Server Error")
+}
+
 app.get('/', async (req: Request, res: Response) => {
 });
 
 app.post('/user', async (req: Request, res: Response) => {
+    const email = req.body.email
+    const name = req.body.name
+    const password = req.body.password
+    const preUser = await User.findByEmailName(email, name)
+    if(preUser){
+        res.status(400).send("User exists (same email or same name)")
+        return
+    }
     try {
-        let user = new UserModel({
+        let user = new User({
             name: req.body.name,
-            email: req.body.email,
+            email: email,
             avatar: req.body.avatar,
-            password: req.body.password
+            password: password
         })
         user = await user.save()
         res.send(user)
     } catch (e) {
         if (e instanceof Error && isValidationError(e)) {
             res.status(400).send(e.message)
+        } else {
+            serverError(res)
         }
     }
 });
