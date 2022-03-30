54140df4fe7ce6a6c10024e321d563aa7cae198b
Dan <d.lyu@mail.utoronto.ca>
2022-03-30 12:03:12 -0400

    Add bcrypt and password salting

--- backend/models/recipe.ts
+++ backend/models/recipe.ts
@@ -1,18 +1,47 @@
-import { Schema, model } from 'mongoose';
-import connectMongo from "../db/mongoose";
-
+import {Schema, model} from 'mongoose';
+import validator from "validator";
+import {genSalt, hash} from "bcryptjs";
 interface User {
     name: string;
     email: string;
+    password: string;
     avatar?: string;
 }
 
-const schema = new Schema<User>({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    avatar: String
+const UserSchema = new Schema<User>({
+    name: {type: String, required: true},
+    email: {
+        type: String, required: true,
+        minlength: 1,
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: 'Not valid email'
+        }
+    },
+    avatar: String,
+    password: {
+        type: String, required: true,
+        minlength: 6
+    }
 });
 
-export const UserModel = model<User>('User', schema);
+UserSchema.pre('save', function(next) {
+    const user = this; // binds this to User document instance
 
-connectMongo().catch(err => console.log(err));
+    // checks to ensure we don't hash password more than once
+    if (user.isModified('password')) {
+        // generate salt and hash the password
+        genSalt(10, (err, salt) => {
+            hash(user.password, salt, (err, hash) => {
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+})
+
+export const UserModel = model<User>('User', UserSchema)
