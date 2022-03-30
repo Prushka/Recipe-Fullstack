75af4d967b7dc8418424223edb59dc0afa1619cb
Dan <d.lyu@mail.utoronto.ca>
2022-03-30 12:03:55 -0400

    Add register post path

--- backend/server.ts
+++ backend/server.ts
@@ -1,18 +1,36 @@
-import express, { Request, Response } from 'express';
+import express, {Request, Response} from 'express';
 import * as bodyParser from "body-parser";
 import {UserModel} from "./models/recipe";
+import connectMongo from "./db/mongoose";
+
+connectMongo().catch(err => console.log(err))
 
 const app = express()
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+function isValidationError(error: Error) {
+    return error.name === "ValidationError"
+}
 
 app.get('/', async (req: Request, res: Response) => {
-    const user = new UserModel({
-        name: 'Bill',
-        email: 'bill@initech.com',
-        avatar: 'https://i.imgur.com/dM7Thhn.png'
-    })
-    await user.save()
-    res.send(await UserModel.find())
+});
+
+app.post('/user', async (req: Request, res: Response) => {
+    try {
+        let user = new UserModel({
+            name: req.body.name,
+            email: req.body.email,
+            avatar: req.body.avatar,
+            password: req.body.password
+        })
+        user = await user.save()
+        res.send(user)
+    } catch (e) {
+        if (e instanceof Error && isValidationError(e)) {
+            res.status(400).send(e.message)
+        }
+    }
 });
 
 const port = process.env.PORT || 5001
