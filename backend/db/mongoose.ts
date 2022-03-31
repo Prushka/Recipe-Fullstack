6b2a7eea7da25d4ecaed63cb0235207ec2f9d348
Dan <d.lyu@mail.utoronto.ca>
2022-03-31 06:21:42 -0400

    Refactor Mongoose Connection

--- backend/db/mongoose.ts
+++ backend/db/mongoose.ts
@@ -1,5 +1,5 @@
 import { connect } from 'mongoose';
 
-export default async function connectMongo() {
+export default async function connectToMongoDB() {
     await connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/RecipeAPI');
 }
