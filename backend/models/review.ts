8e401ac4ab89512dbf7a90d77c54457888fd7b64
Dan <d.lyu@mail.utoronto.ca>
2022-03-31 11:52:53 -0400

    Add user author mapping to Review

--- backend/models/review.ts
+++ backend/models/review.ts
@@ -4,6 +4,7 @@ interface IReview extends Document {
     title: string
     content: string
     reviewedRecipe: ObjectId
+    author: ObjectId
     rating: 1 | 0 | -1 // according to whoever that person is, we are doing thumbs up/down only
 }
 
@@ -11,7 +12,8 @@ const ReviewSchema = new Schema<IReview>({
     title: {type: String, required: true},
     content: {type: String, required: true, default: ""},
     reviewedRecipe: {type: String, required: true},
-    rating: {type: Number, required: true, default: 0}
+    rating: {type: Number, required: true, default: 0},
+    author: {type: String, required: true}
 });
 
 
