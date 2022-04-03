f1ac1d985bf18cb94f46d1a9058a4294d6ce3a4a
Dan <d.lyu@mail.utoronto.ca>
2022-04-03 13:47:10 -0400

    Implement report post/Update review schema

--- backend/models/review.ts
+++ backend/models/review.ts
@@ -1,7 +1,9 @@
 import {Document, Model, model, ObjectId, Schema} from "mongoose";
 
-interface IUserReviewVote extends Document {
-    positivity: boolean
+type SimpleRating = 1 | 0 | -1
+
+interface IUserReviewVote {
+    positivity: SimpleRating
     author: ObjectId
 }
 
@@ -9,26 +11,28 @@ export interface IReview extends Document {
     userVotes: IUserReviewVote[]
     inappropriateReportUsers: ObjectId[]
     approved: boolean
-    title: string
     content: string
     reviewedRecipe: ObjectId
     author: ObjectId
-    rating: 1 | 0 | -1 // according to whoever that person is, we are doing thumbs up/down only
+    rating: SimpleRating // according to whoever that person is, we are doing thumbs up/down only
 }
 
 const UserReviewVoteSchema = new Schema<IUserReviewVote>({
-    positivity: {type: "boolean", required: true},
+    positivity: {
+        type: Number, required: true, default: 0, min: -1, max: 1,
+        get: (v: number) => Math.round(v),
+        set: (v: number) => Math.round(v)
+    },
     author: {type: String, required: true}
 })
 
 const ReviewSchema = new Schema<IReview>({
-    title: {type: String, required: true},
     content: {type: String, required: true, default: ""},
     reviewedRecipe: {type: String, required: true},
     rating: {
         type: Number, required: true, default: 0, min: -1, max: 1,
         get: (v: number) => Math.round(v),
-        set: (v: number) => Math.round(v),
+        set: (v: number) => Math.round(v)
     },
     author: {type: String, required: true},
     approved: {type: Boolean, required: true, default: true},
--- backend/routes/review.ts
+++ backend/routes/review.ts
@@ -3,44 +3,57 @@
  */
 
 import {idToObjectId, requireObjectIdFromPara} from "../utils/util";
-import {Recipe} from "../models/recipe";
 import {IReview, Review} from "../models/review";
 import express from "express";
 import {userRoute} from "./route";
 import {IUser, Role} from "../models/user";
-import {ObjectId} from "mongoose";
+import {ObjectId as ObjectIdType} from "mongoose";
 import {EndpointError, throwError} from "../errors/errors";
+
+const {ObjectId} = require('mongodb');
 
 export const reviewRouter = express.Router()
 
-async function requiredReviewFromId(id: ObjectId): Promise<IReview> {
+async function requireReviewFromId(id: ObjectIdType): Promise<IReview> {
     const review = await Review.findById(id)
     if (!review) {
-        throwError(EndpointError.RecipeNotFound)
+        throwError(EndpointError.ReviewNotFound)
     }
     return review!
 }
 
-function requireReviewEdit(actor:IUser, review:IReview) {
-    if(review.author !== actor._id && actor.role < Role.ADMIN){
+function requireReviewEdit(actor: IUser, review: IReview) {
+    if (review.author !== actor._id && actor.role < Role.ADMIN) {
         throwError(EndpointError.NoPermission)
     }
 }
 
+reviewRouter.post('/vote/:id', userRoute(async (req, res, sessionUser) => {
+    const reviewId = requireObjectIdFromPara(req)
+    await requireReviewFromId(reviewId)
+    const reviewVote = {
+        positivity: req.body.positivity ?? 0,
+        author: sessionUser._id
+    }
+    const review = await Review.findByIdAndUpdate(
+        reviewId,
+        {$addToSet: {userVotes: reviewVote}},
+        {new: true})
+    res.send(review)
+}))
+
 reviewRouter.delete('/:id', userRoute(async (req, res, sessionUser) => {
     const reviewId = requireObjectIdFromPara(req)
-    let review = await requiredReviewFromId(reviewId)
+    let review = await requireReviewFromId(reviewId)
     requireReviewEdit(sessionUser, review)
     await review.delete()
     res.send("deleted")
 }))
 
-
 reviewRouter.patch('/:id', userRoute(async (req, res, sessionUser) => {
     const reviewId = requireObjectIdFromPara(req)
-    let review = await requiredReviewFromId(reviewId)
+    let review = await requireReviewFromId(reviewId)
     requireReviewEdit(sessionUser, review)
-    review.title = req.body.title ?? review.title
     review.content = req.body.content ?? review.content
     review.rating = req.body.rating ?? review.rating
     review = await review.save()
@@ -48,12 +61,8 @@ reviewRouter.patch('/:id', userRoute(async (req, res, sessionUser) => {
 }))
 
 reviewRouter.post('/', userRoute(async (req, res) => {
-    const recipeId = idToObjectId(req.body.reviewedRecipe)
-    const recipe = await Recipe.findById(recipeId)
-    if (!recipe) {
-        res.status(404).send("Recipe not found")
-        return
-    }
+    const id = idToObjectId(req.body.reviewedRecipe)
+    await requireReviewFromId(id)
     const preReview = await Review.findOne({author: req.session.user!._id})
     if (preReview) {
         res.status(400).send("You already have one review on this recipe (you can update it tho)")
