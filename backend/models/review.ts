2492efdf5c7479cb2327e16ddda70b130ecda4a5
Dan <d.lyu@mail.utoronto.ca>
2022-04-03 13:08:45 -0400

    Add Report/Review Voting schema and interface

--- backend/models/review.ts
+++ backend/models/review.ts
@@ -1,12 +1,25 @@
 import {Document, Model, model, ObjectId, Schema} from "mongoose";
 
+interface IUserReviewVote extends Document {
+    positivity: boolean
+    author: ObjectId
+}
+
 interface IReview extends Document {
+    userVotes: IUserReviewVote[]
+    inappropriateReportUsers: ObjectId[]
+    approved: boolean
     title: string
     content: string
     reviewedRecipe: ObjectId
     author: ObjectId
     rating: 1 | 0 | -1 // according to whoever that person is, we are doing thumbs up/down only
 }
+
+const UserReviewVoteSchema = new Schema<IUserReviewVote>({
+    positivity: {type: "boolean", required: true},
+    author: {type: String, required: true}
+})
 
 const ReviewSchema = new Schema<IReview>({
     title: {type: String, required: true},
@@ -17,7 +30,10 @@ const ReviewSchema = new Schema<IReview>({
         get: (v: number) => Math.round(v),
         set: (v: number) => Math.round(v),
     },
-    author: {type: String, required: true}
+    author: {type: String, required: true},
+    approved: {type: Boolean, required: true, default: true},
+    userVotes: [{type: UserReviewVoteSchema}],
+    inappropriateReportUsers: [{type: String}]
 });
 
 
