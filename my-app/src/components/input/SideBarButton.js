9da9e978e93cbd1a155b1fae6fb530864a4d1f19
Dan <d.lyu@mail.utoronto.ca>
2022-04-04 12:37:47 -0400

    Fetch user info on sidebar change

--- my-app/src/components/input/SideBarButton.js
+++ my-app/src/components/input/SideBarButton.js
@@ -5,19 +5,34 @@
 import React from 'react';
 import '../../styles/Sidebar.css';
 import {useNavigate} from 'react-router-dom';
+import {useDispatch, useSelector} from "react-redux";
+import {UserAPI} from "../../axios/Axios";
+import {setUser} from "../../redux/Redux";
 
-export default function SideBarButton({onClick=()=>{}, setSideBarOpen, title, icon, path, isSelected}) {
+export default function SideBarButton({
+                                          onClick = () => {
+                                          }, setSideBarOpen, title, icon, path, isSelected
+                                      }) {
     const navigate = useNavigate()
+    const user = useSelector((state) => state.user)
+    const dispatch = useDispatch()
     return (
         <>
             <div className={`side-bar__button ${isSelected(path) && 'side-bar__button--selected'}`}
-                    onClick={() => {
-                        onClick()
-                        if(path){
-                            setSideBarOpen(false)
-                            navigate(path)
-                        }
-                    }}><div className={'side-bar__icon'}>{icon}</div>{title}
+                 onClick={async () => {
+                     onClick()
+                     if (path) {
+                         await UserAPI.get('', {withCredentials: true}).then(res => {
+                             dispatch(setUser(res.data))
+                         }).catch(e => {
+                             navigate('/login')
+                         })
+                         setSideBarOpen(false)
+                         navigate(path)
+                     }
+                 }}>
+                <div className={'side-bar__icon'}>{icon}</div>
+                {title}
             </div>
         </>
 
