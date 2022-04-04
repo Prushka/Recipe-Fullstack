c704d4ef4b3479c55dc8fc2b7f1bd1fb77958557
Dan <d.lyu@mail.utoronto.ca>
2022-04-04 10:08:37 -0400

    Use form in login page

--- my-app/src/pages/login/Login.js
+++ my-app/src/pages/login/Login.js
@@ -9,43 +9,51 @@ import {useNavigate} from "react-router-dom";
 import {useSelector, useDispatch} from 'react-redux'
 import {login} from '../../redux/Redux'
 import {UserAPI} from "../../axios/Axios";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Alert, Snackbar} from "@mui/material";
 import {useSnackbar} from "notistack";
 
 export default function Login() {
-    const { enqueueSnackbar, closeSnackbar } = useSnackbar()
+    const {enqueueSnackbar, closeSnackbar} = useSnackbar()
     const navigate = useNavigate()
     const user = useSelector((state) => state.user)
     const dispatch = useDispatch()
     const [usernameEmail, setUsernameEmail] = useState("")
     const [password, setPassword] = useState("")
 
-
-    const login = async () => {
+    const login = async (usernameEmail, password) => {
         await UserAPI.post('/login',
-            {"email": usernameEmail, "password": password}).then(res => {
+            {"input": usernameEmail, "password": password}).then(res => {
+            enqueueSnackbar(`Success`,
+                {
+                    variant: 'success'
+                })
             console.log(res)
         }).catch(error => {
-            enqueueSnackbar(`${error.response.data}`,
+            enqueueSnackbar(`${error.response.data.message}`,
                 {
                     variant: 'error'
                 })
         })
     }
+
     return (
         <>
             <div className="auth__container">
-                <div className="auth__title">
-                    Log-in
-                </div>
-                <TextField value={usernameEmail} setValue={setUsernameEmail} type="username" className="auth__input"
-                           label={'Username'}/>
-                <TextField value={password} setValue={setPassword} type="password" className="auth__input"
-                           label={'Password'}/>
-                <BlueBGButton className="auth__button" onClick={async () => {
-                    await login()
-                }}>Login</BlueBGButton>
+                <form onSubmit={async(e)=>{
+                    e.preventDefault()}}>
+                    <div className="auth__title">
+                        Log-in
+                    </div>
+                    <TextField value={usernameEmail} setValue={setUsernameEmail} type="username" className="auth__input"
+                               label={'Username / Email'}/>
+                    <TextField value={password} setValue={setPassword} type="password" className="auth__input"
+                               label={'Password'}/>
+                    <BlueBGButton type="submit" className="auth__button" onClick={async () => {
+                        await login(usernameEmail, password)
+                    }}>Login</BlueBGButton>
+
+                </form>
                 <div onClick={() => {
                     navigate("/signup")
                 }} className="auth__link">Don't have an account? Click here to sign up
