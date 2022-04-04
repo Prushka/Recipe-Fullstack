197ed679058d156abb597a4dae273071f6ccdb84
Dan <d.lyu@mail.utoronto.ca>
2022-04-04 08:06:11 -0400

    Rewrite signup page/Add navigation

--- my-app/src/pages/login/Login.js
+++ my-app/src/pages/login/Login.js
@@ -5,8 +5,10 @@ import './Auth.css';
 import {TextField} from "../../components/input/TextField";
 import * as React from "react";
 import {BlueBGButton} from "../../components/input/Button";
+import {useNavigate} from "react-router-dom";
 
 export default function Login() {
+    const navigate = useNavigate()
     return (
         <>
         <div className="auth__container">
@@ -17,7 +19,9 @@ export default function Login() {
             <TextField type="password" className="auth__input" label={'Password'}/>
                 <BlueBGButton className="auth__button" onClick={() => {
                 }}>Login</BlueBGButton>
-            <div className="auth__link">Click here to create an account</div>
+            <div onClick={() => {
+                navigate("/signup")
+            }} className="auth__link">Don't have an account? Click here to sign up</div>
         </div>
         </>
     )
--- my-app/src/pages/login/Signup.js
+++ my-app/src/pages/login/Signup.js
@@ -2,15 +2,27 @@
  * Copyright 2022 Dan Lyu.
  */
 import './Auth.css';
+import {TextField} from "../../components/input/TextField";
+import {BlueBGButton} from "../../components/input/Button";
+import * as React from "react";
+import {useNavigate} from "react-router-dom";
 
 export default function Signup() {
+    const navigate = useNavigate()
     return (
         <>
-        <div className="auth__container">
-            <div className="auth__title">
-                Log-in
+            <div className="auth__container">
+                <div className="auth__title">
+                    Signup
+                </div>
+                <TextField type="username" className="auth__input" label={'Username'}/>
+                <TextField type="password" className="auth__input" label={'Password'}/>
+                <BlueBGButton className="auth__button" onClick={() => {
+                }}>Login</BlueBGButton>
+                <div onClick={() => {
+                    navigate("/login")
+                }} className="auth__link">Already have an account? Click here to login</div>
             </div>
-        </div>
         </>
     )
 }
