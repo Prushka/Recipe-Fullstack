1998c20cfb5020c69001563f8e58ca4098433522
Dan <d.lyu@mail.utoronto.ca>
2022-04-04 14:12:26 -0400

    Fix TextField onChange

--- my-app/src/components/input/TextField.js
+++ my-app/src/components/input/TextField.js
@@ -5,7 +5,21 @@
 import * as React from 'react';
 import './Input.css';
 
-export function TextField({disabled=false, textFieldClassName='', children, type='text', label, id, className, value, setValue, placeholder="", name}) {
+export function TextField({
+                              onChange = () => {
+                              },
+                              disabled = false,
+                              textFieldClassName = '',
+                              children,
+                              type = 'text',
+                              label,
+                              id,
+                              className,
+                              value,
+                              setValue,
+                              placeholder = "",
+                              name
+                          }) {
     return (
         <div className={`textfield-section ${textFieldClassName}`}>
             <div className={`textfield-header`}>
@@ -13,7 +27,12 @@ export function TextField({disabled=false, textFieldClassName='', children, type
                 {children}
             </div>
             <input value={value} disabled={disabled}
-                   onChange={e => setValue(e.target.value)}
+                   onChange={e => {
+                       if (setValue) {
+                           setValue(e.target.value)
+                       }
+                       onChange(e)
+                   }}
                    className={`textfield-section__input ${className}`} id={id} name={name}
                    placeholder={placeholder}
                    type={type}
